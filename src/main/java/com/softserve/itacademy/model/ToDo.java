dc7276d39d00767704a4582f6f6fcb0ff799f8a6
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2026-06-30 19:48:59 +0200

    complete assignment

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -2,19 +2,45 @@ package com.softserve.itacademy.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = {"owner", "tasks", "collaborators"})
 public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String title;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
+
+    @OneToMany(mappedBy = "todo")
     private List<Task> tasks;
+
+    @ManyToMany
+    @JoinTable(
+            name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "todo_id"),
+            inverseJoinColumns = @JoinColumn(name = "collaborator_id")
+    )
     private List<User> collaborators;
 
 }
--- src/main/java/com/softserve/itacademy/model/User.java
+++ src/main/java/com/softserve/itacademy/model/User.java
@@ -2,16 +2,57 @@ package com.softserve.itacademy.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import java.util.List;
-import java.util.Objects;
+import lombok.*;
 
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = {"myTodos", "otherTodos"})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "First name must start with a capital letter followed by lowercase letters, optionally separated by a hyphen")
+    @Pattern(
+            regexp = "^[A-Z][a-z]+(-[A-Z][a-z]+)*$",
+            message = "First name must start with a capital letter followed by lowercase letters, optionally separated by a hyphen"
+    )
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @NotNull(message = "Last name must start with a capital letter followed by lowercase letters, optionally separated by a hyphen")
+    @Pattern(
+            regexp = "^[A-Z][a-z]+(-[A-Z][a-z]+)*$",
+            message = "Last name must start with a capital letter followed by lowercase letters, optionally separated by a hyphen"
+    )
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Email(message = "Must be a valid e-mail address")
+    @NotBlank(message = "Must be a valid e-mail address")
+    @Column(nullable = false)
     private String email;
+
+    @NotNull(message = "Password must be at least 8 characters long and contain at least one letter, one number, and can include special characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+\\[\\]{}|;:'\",.<>?/`~\\\\]).{8,}$",
+            message = "Password must be at least 8 characters long and contain at least one letter, one number, and can include special characters"
+    )
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @OneToMany(mappedBy = "owner")
     private List<ToDo> myTodos;
+
+    @ManyToMany(mappedBy = "collaborators")
     private List<ToDo> otherTodos;
 }
